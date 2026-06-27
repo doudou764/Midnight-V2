9b9a71db6d6091a24092a7d64f265dddbd807b63
doudou764 <140897081+doudou764@users.noreply.github.com>
2026-06-27 13:23:58 +0200

    Define Product type with relevant fields

--- types/product.ts
+++ types/product.ts
@@ -1,1 +1,10 @@
-
+export type Product = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  category: "game" | "subscription" | "giftcard" | "software";
+  stock: number;
+  created_at?: string;
+};
